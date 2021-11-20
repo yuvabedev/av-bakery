ba933b87389fc282864d35698b33815af2b4f6c1
Pawan Mittal <pmittal@directcommerce.com>
2021-11-20 15:15:07 +0530

    day of the week dropdown is autofilled when user selects an order start date

--- static/scripts/validate_order.js
+++ static/scripts/validate_order.js
@@ -1,3 +1,13 @@
+const weekdays = new Array(7);
+weekdays[0] = "Sunday";
+weekdays[1] = "Monday";
+weekdays[2] = "Tuesday";
+weekdays[3] = "Wednesday";
+weekdays[4] = "Thursday";
+weekdays[5] = "Friday";
+weekdays[6] = "Saturday";
+
+
 $("#orderDeliveryDays").mouseover(function() {
     if ($("#orderDeliveryDaysDropdown").is(':disabled')) {
         $("#orderDeliveryDays").css('cursor','pointer').attr('title', 'First select order start date');
@@ -10,3 +20,46 @@ $("#orderDeliveryDays").mouseover(function() {
           }, 2000);
     }
 });
+
+$('#startDate').change(function() {
+    var startDate = $('#startDate').val();
+    if (startDate == null || startDate.length < 1) {
+        disableOrderDeliveryDaysDropdown();
+        autoPopulateOrderDeliveryDate('');
+        return;
+    };
+
+    console.log("Order start date updated to "  +startDate);
+    enableOrderDeliveryDaysDropdown();
+    var dayOfWeek = getWeekDayFromDate(startDate);
+    console.log(`${startDate} falls on ${dayOfWeek}`);
+    autoPopulateOrderDeliveryDate(dayOfWeek);
+});
+
+
+function enableOrderDeliveryDaysDropdown() {
+    $("#orderDeliveryDaysDropdown").prop("disabled", false);
+}
+
+function disableOrderDeliveryDaysDropdown() {
+    $("#orderDeliveryDaysDropdown").prop("disabled", true);
+
+}
+/** Auto populates the order delivery day dropdown by adding the week day that corresponds to the order start date selected by user */
+function autoPopulateOrderDeliveryDate(dayOfWeek) {
+    $("#orderDeliveryDaysDropdown").val(dayOfWeek).trigger('change');
+}
+
+/** Returns the week day on which date string falls on. For example, 20/11/2021 falls on a Saturday 
+ * so, Satudarday is returned.
+*/
+
+function getWeekDayFromDate(dateString) {
+    var dateAttributes = dateString.split("/");
+    //dateAttributes[0] => day of the month, dateAttributes[1] => month, dateAttributes[2] => year
+    var parsableDate = `${dateAttributes[1]}/${dateAttributes[0]}/${dateAttributes[2]}`;
+    var timestamp = Date.parse(parsableDate);
+    var dateObject = new Date(timestamp);
+    var dayOfWeek = weekdays[dateObject.getDay()];
+    return dayOfWeek;
+}
